2e8d42b1b9cfd2aed76c8087875910ba7300d66f
plotinusspascual <plotinusspascual@gmail.com>
2022-02-03 00:50:21 -0800

    added prompts and functions to add,view,update employees and departments

--- server.js
+++ server.js
@@ -14,3 +14,36 @@ app.use(express.json());
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+function prompt(){
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      name: "choice",
+      choices: 
+      [
+        "View All Employees?",
+        "View All Employee's By Roles?",
+        "View All Employee's By Departments",
+        "Update Employee",
+        "Add Employee?",
+        "Add Role?",
+        "Add Department?"   
+      ]
+    }
+  ]).then(function(val){
+    switch(val.choice){
+      case "View All Employees":
+        viewEmployees();
+    }
+  })
+}
+
+function viewEmployees(){}
+function viewRoles(){}
+function viewDepartments(){}
+function updateEmployee(){}
+function addEmployee(){}
+function addRole(){}
+function addDepartment(){}
